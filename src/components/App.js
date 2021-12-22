97385084821968776a48f671c9285c3b1feb3f9a
Juan Patricio Marroquin <review0312oficial@gmail.com>
2021-12-21 22:25:44 -0500

    Red de rinkeby configurada

--- src/components/App.js
+++ src/components/App.js
@@ -32,7 +32,7 @@ class App extends Component {
     this.setState({account: accounts[0]});
     console.log('account: ', this.state.account);
     
-    const networkId = '5777';
+    const networkId = '4'; // Rinkeby: 4 | Ganache: 5777
     console.log('networkId: ', networkId);
     const networkData = contratoToken.networks[networkId];
     console.log('networkData:', networkData);
--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,23 @@
 require('babel-register');
 require('babel-polyfill');
 
+var HDWalletProvider = require('truffle-hdwallet-provider');
+var mnemonic = "...";
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/086e876c029a4e1c90a1a3c3d3821b61")
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
     },
   },
   contracts_directory: './src/contracts/',
